3d8ffd016af76788ca0a2e8d2305c07c58c9a2d7
DineshMurugan2022 <2002dineshmurugan@gmail.com>
2026-07-29 14:59:55 +0530

    fix: update Google Maps embed location on Contact page to exact Dr.Well Care Mattress pin

--- apps/web/src/app/contact/page.tsx
+++ apps/web/src/app/contact/page.tsx
@@ -316,7 +316,7 @@ export default function ContactPage() {
         </div>
         <div className="w-full h-[450px] rounded-3xl overflow-hidden shadow-xl border border-slate-200">
           <iframe 
-            src="https://maps.google.com/maps?q=Surapet,%20Chennai&t=&z=14&ie=UTF8&iwloc=&output=embed" 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3910.605674547909!2d80.1844535750794!3d13.14279238718852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52649df083458f%3A0x570c69f166e4dc62!2sDr.Well%20Care%20Mattress!5e1!3m2!1sen!2sin!4v1785317138440!5m2!1sen!2sin" 
             width="100%" 
             height="100%" 
             style={{ border: 0 }} 
